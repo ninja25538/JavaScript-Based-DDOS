b60cf0c1fa89b5741ed2d12baf617c44a0ba4a21
ninja25538 <ninja25538@users.noreply.github.com>
2017-08-04 12:04:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+setInterval(function(){
+var randomNumber1 = Math.floor((Math.random() * 9001) + 1);
+var randomNumber2 = Math.floor((Math.random() * 9001) + 1);
+var x = document.createElement("IFRAME");
+myPara.setAttribute("x", randomNumber1);
+var y = document.getElementById("myFrame").src;
+}, 100);
