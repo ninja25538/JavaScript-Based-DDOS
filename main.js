33458da6b6da3a9c4ea15dc2a6e92f10d8a779e6
ninja25538 <ninja25538@users.noreply.github.com>
2017-08-04 12:16:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ var randomNumber1 = Math.floor((Math.random() * 9001) + 1);
 var randomNumber2 = Math.floor((Math.random() * 9001) + 1);
 var x = document.createElement("IFRAME");
 myPara.setAttribute("x", randomNumber1);
-var y = document.getElementById("myFrame").src;
+var y = document.getElementById("myFrame").src = "http://www.mvcsc.k12.in.us/userfiles/2/my%20files/district/2017-2018/district/2017-2018%20mvhs%20student%20handbook.pdf?id=5133?r="+randomNumber2;
+ 
 }, 100);
