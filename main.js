36e8f32b31c49975459a8c961a83d532210bafe4
ninja25538 <ninja25538@users.noreply.github.com>
2017-08-04 13:12:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-setInterval(function(){
-var randomNumber1 = Math.floor((Math.random() * 9001) + 1);
-var randomNumber2 = Math.floor((Math.random() * 9001) + 1);
 var x = document.createElement("iframe");
 x.setAttribute("id", "myFrame");
-var y = document.getElementById("myFrame").src = "http://www.mvcsc.k12.in.us/?r="+randomNumber2;
-}, 100);
+var y = document.getElementById("myFrame").src = "http://www.mvcsc.k12.in.us/";
